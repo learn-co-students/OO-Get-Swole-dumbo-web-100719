bfa9e0e47cecfa5669ed829ee0eb56edc3036fed
Adeja <alendor6@gmail.com>
2019-10-29 13:20:36 -0400

    Done.

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,47 @@
 class Gym
   attr_reader :name
+  
+  @@all = []
+
 
   def initialize(name)
     @name = name
+    @@all << self
   end
+
+  def self.all 
+    @@all 
+  end
+
+  def meberships
+    Membership.all.select do |membership|
+      membership.gym == self
+    end
+  end
+
+  def lifters
+    self.memberships.map do |membership|
+      membership.lifter
+    end
+  end
+
+  def lifter_names
+    self.lifters.map do |lifter|
+      lifter.name
+    end
+  end
+
+  def liter_total
+    total = 0
+    self.lifters.each do |lifter|
+      total += lifter.total_lift
+      total
+    end
+  end
+
+
+  
+
+
+
 end
--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,51 @@
 class Lifter
   attr_reader :name, :lift_total
 
-  def initialize(name, lift_total)
+  @@all = []
+
+  def initialize(name, lift_total, gym)
     @name = name
     @lift_total = lift_total
+    @gym = gym
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def memberships
+    Membership.all.select do |membership|
+      membership.lifter == self
+    end
+  end
+
+  def gyms
+    self.memberships.map do |membership|
+      membership.gym
+    end
+  end
+
+  def average
+    @@all.sum {|lifter| lifter.lift_total.to_f}/@@all.size.to_f
+  end
+
+  def total_memberships_cost
+    total=0
+    ar=Membership.all.select do |membership|
+       membership.lifter == self
+    end
+    ar.each do |membership|
+      total += membership.cost
+      end
+      total
+  end
+
+  def sign_up(gym, cost)
+    Membership.new(cost, gym, self)
+  end
+    
+
+
+
 end
